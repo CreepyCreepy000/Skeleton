9e69cc27d11696c052e370fc6246a82e8492121f
licjcb8 <38548801+licjcb8@users.noreply.github.com>
2018-08-22 15:57:01 +0900

    a

---
+++ Skeleton/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    Idle,
+    Left,
+    Right
+}
+
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour {
+
+    public PlayerState PS;
+    public Animator animator;
+
+    public float moveSpeed = 5;
+    PlayerController controller;
+
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+    }
+
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        controller.Move(moveVelocity);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+        }
+    }
+
+    void Left()
+    {
+        PS = PlayerState.Left;
+        animator.SetTrigger("Left");
+    }
+    void Right()
+    {
+        PS = PlayerState.Right;
+        animator.SetTrigger("Right");
+    }
+
+}
